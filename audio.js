2d943c43707a2872664609322d59ccde342c871e
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2019-05-31 22:13:11 +0200

    Play sound when key is pressed

--- audio.js
+++ audio.js
@@ -3,15 +3,13 @@ const context = new window.AudioContext();
 const amp = context.createGain();
 amp.connect(context.destination);
 
-const oscillator = context.createOscillator();
-oscillator.start();
-oscillator.connect(amp);
-
-const lfoDepth = context.createGain();
-lfoDepth.gain.value = 0.5;
-lfoDepth.connect(amp.gain);
-
-const lfo = context.createOscillator();
-lfo.frequency.value = 0.5;
-lfo.connect(lfoDepth);
-lfo.start();
+document.addEventListener("keypress", (event) => {
+    if (event.key === "a") {
+        const oscillator = context.createOscillator();
+        oscillator.connect(amp);
+        oscillator.start();
+        setTimeout(() => {
+            oscillator.stop();
+        }, 1000);
+    }
+});
