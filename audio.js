781c13d9d6bea5e16332e074bf1ffb508efefe91
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2019-05-31 22:31:01 +0200

    Allow input from multiple keys

--- audio.js
+++ audio.js
@@ -1,11 +1,32 @@
+function midiToFrequency(noteNumber) {
+    return Math.pow(2, (noteNumber - 69) / 12) * 440;
+}
+
+const FREQUENCIES = {
+    "a": midiToFrequency(60), // C
+    "w": midiToFrequency(61), // C#
+    "s": midiToFrequency(62), // D
+    "e": midiToFrequency(63), // D#
+    "d": midiToFrequency(64), // E
+    "f": midiToFrequency(65), // F
+    "t": midiToFrequency(66), // F#
+    "g": midiToFrequency(67), // G
+    "y": midiToFrequency(68), // G#
+    "h": midiToFrequency(69), // A
+    "u": midiToFrequency(70), // A#
+    "j": midiToFrequency(71), // B
+    "k": midiToFrequency(72), // C
+}
+
 const context = new window.AudioContext();
 
 const amp = context.createGain();
 amp.connect(context.destination);
 
 document.addEventListener("keypress", (event) => {
-    if (event.key === "a") {
+    if (FREQUENCIES.hasOwnProperty(event.key)) {
         const oscillator = context.createOscillator();
+        oscillator.frequency.value = FREQUENCIES[event.key];
         oscillator.connect(amp);
         oscillator.start();
         setTimeout(() => {
