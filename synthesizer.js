cd1ba188039b7d5255014e51a227f66d0eecf538
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2019-06-01 23:47:40 +0200

    Monophonic synthesizer

--- synthesizer.js
+++ synthesizer.js
@@ -22,15 +22,17 @@ const context = new window.AudioContext();
 
 const amp = context.createGain();
 amp.connect(context.destination);
+amp.gain.setValueAtTime(0, context.currentTime);
+
+const oscillator = context.createOscillator();
+oscillator.connect(amp);
+oscillator.start();
 
 document.addEventListener("keypress", (event) => {
     if (FREQUENCIES.hasOwnProperty(event.key)) {
-        const oscillator = context.createOscillator();
+        amp.gain.cancelScheduledValues(context.currentTime);
         oscillator.frequency.value = FREQUENCIES[event.key];
-        oscillator.connect(amp);
-        oscillator.start();
-        setTimeout(() => {
-            oscillator.stop();
-        }, 1000);
+        amp.gain.setValueAtTime(1, context.currentTime);
+        amp.gain.setTargetAtTime(0, context.currentTime + 0.25, 0.25);
     }
 });
